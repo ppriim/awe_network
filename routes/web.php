21860e15fa8c210611de9ea82945452981ed249f
ppriim <rachanika.c@gmail.com>
2025-04-30 08:32:52 +0700

    debug route

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    try {
+        return view('home');
+    } catch (\Throwable $e) {
+        return $e->getMessage();
+    }
 });
+
 
 Route::get('/about', function () {
     return view('about');
